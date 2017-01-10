68026f838f5502c582322ba94c389a6149d05990
Muhammad Hassaan Kamran <MuhammadHassaan.Kamran@afiniti.com>
2017-01-10 20:04:37 +0500

    dev 2 messages added

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -20,6 +20,10 @@ namespace WindowsFormsApplication1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("original text");
+
+            MessageBox.Show("Dev 2 => Message 2.1");
+            MessageBox.Show("Dev 2 => Message 2.2");
+            MessageBox.Show("Dev 2 => Message 2.3");
         }
     }
 }
