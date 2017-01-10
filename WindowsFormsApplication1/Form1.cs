3402fce49913bb203b5425fb125e6f6c3312d360
hassaankamran <hassaankamran@gmail.com>
2017-01-10 22:21:47 +0500

    Update Form1.cs

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -20,10 +20,6 @@ namespace WindowsFormsApplication1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("original text");
-			MessageBox.Show("Dev2 messages => 2.1");
-            MessageBox.Show("Dev2 messages => 2.2");
-            MessageBox.Show("Dev2 messages => 2.3");			MessageBox.Show("Dev1 messages => 1.1");
-            MessageBox.Show("Dev1 messages => 1.2");
-            MessageBox.Show("Dev1 messages => 1.3");        }
+	    }
     }
 }
